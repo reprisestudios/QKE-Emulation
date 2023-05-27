d6caf8c22524e3f33f9994eac64d972f34fb9863
Espi <ethyn.gillies@gmail.com>
2023-05-27 16:53:03 +1200

    Reciever class and reading qubits Qubits can be measured in the reciever class by selecting a random polarisation and measuing using that polarisation.

---
+++ Receiver.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Receiver {
+    int[] bits, polarisations;
+
+    public void readQubits(Qubit[] qubits) {
+        Random rnd = new Random();
+        bits = new int[qubits.length];
+        polarisations = new int[qubits.length];
+
+        for (int i = 0; i < qubits.length; i++) {
+            polarisations[i] = rnd.nextInt(1);
+
+            bits[i] = qubits[i].measure(polarisations[i]);
+        }
+    }
+}
